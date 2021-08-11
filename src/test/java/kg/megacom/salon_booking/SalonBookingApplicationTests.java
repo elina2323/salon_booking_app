bce4396e75681aa54b2224dd359025e33e2eedab
elina2323 <elina.balabaeva2323@gmail.com>
2021-08-11 10:08:31 +0600

    Initial commit

---
+++ src/test/java/kg/megacom/salon_booking/SalonBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.salon_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
